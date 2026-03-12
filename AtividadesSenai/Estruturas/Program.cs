4316945f4f4f77761f1fe26bd5f28d06da801d7f
Vitor <vitormateushd123@gmail.com>
2026-03-12 13:51:12 -0300

    nada
    nada

---
+++ AtividadesSenai/Estruturas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estruturas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vetor vetor = new Vetor();
+
+            vetor.Menu();
+        }
+    }
+}
