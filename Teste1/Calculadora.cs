6121590c814bd898887783e795f277bb54859ddc
Vitor <vitormateushd123@gmail.com>
2026-05-14 14:45:17 -0300

    Add files via upload

---
+++ Teste1/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teste1
+{
+    public class Calculadora
+    {
+        public int Somar(int x, int y) 
+        {
+            return x + y;   
+        }
+
+		public int Subtrair(int x, int y)
+		{
+			return x - y;
+		}
+
+	}
+}
---
+++ Teste1/Login.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teste1;
+
+public class Login
+{
+	public List<(string username, string password)> ListaUsuarios;
+
+	public Login()
+	{
+		ListaUsuarios = new List<(string username, string password)>{
+		("adm","adm"),
+		("123","123"),
+		("222","222")
+		};
+	}
+	public bool Logar(string username, string password, bool capcha)
+	{
+		if (!capcha || username is null || password is null) 
+		{
+			return false;
+		}
+
+		foreach (var u in ListaUsuarios) 
+		{
+			if (u.username == username && u.password == password) 
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
+
