83464f96924474803322af088c3b63ebabb90b9a
Vitor <vitormateushd123@gmail.com>
2026-05-14 16:00:25 -0300

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Loja;
+
+namespace Testes;
+
+public class UnitTest1
+{
+    private readonly F_TelaLogin _fLogin = new F_TelaLogin();
+    
+    [Fact]
+    public void TeteHash()
+    {
+        string senha = "123";
+        string senhaHash = senha.Hash();
+
+        Assert.Equal(true, Hasher.Verify(senha, senhaHash));
+    }
+
+    [Fact]
+    public void TesteLogin() 
+    {
+       var resultado =  _fLogin.Login("vitor123","123");
+
+        Assert.Equal(true ,resultado);
+    }
+}
+
